86139568e8d0ec291e9481855e9c9fd4e116ea8e
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-03-29 23:45:05 -0300

    add comentários

--- JavaFiles/QueryExecutor.java
+++ JavaFiles/QueryExecutor.java
@@ -10,11 +10,12 @@ public class QueryExecutor {
 
     // método que executa um selec simples
     public static void executeSimpleQuery(Connection conexao) throws SQLException {
+        // string que carrega o comando em sql
         String sql = "SELECT * FROM tabela";
+        // execução da query
         try (PreparedStatement statement = conexao.prepareStatement(sql)) {
             ResultSet result = statement.executeQuery();
             // processa o resultado aqui...
         }
     }
-
 }
